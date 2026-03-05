3c21cf691f4d5cd1b563e00194c622a5d87c1817
Deepa Hospital <deploy@deepahospital.org>
2026-03-05 15:11:04 +0530

    Deploy to GitHub Pages

--- _next/static/chunks/app/departments/gynecology/page-f7d02a944a86ef63.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[46,220,519,385,571],{2561:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=2561)}),_N_E=n.O()}]);
